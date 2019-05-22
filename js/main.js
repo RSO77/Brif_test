86e9b3b69134a0685db8c29ec5eaef07d827309f
rso <Ostapchenko.roma@gmail.com>
2019-05-22 13:36:08 +0300

    RSO

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@
 $(function () {
     $('.option_block__js').click(function () {
         $(this).toggleClass('option_block__active');
-        console.log(1);
+        $(this).toggleClass('animated');
+        $(this).toggleClass('flip');
     });
+
+    // $('.option_block__js_hover').hover(
+    //     function(){ $(this).addClass('animated'); $(this).addClass('flip') },
+    //     function(){ $(this).removeClass('animated'); $(this).removeClass('flip') }
+    // );
 });
 
